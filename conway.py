c6bc6130c726a04eeb6b67d64756e5ab6c26508c
Pacman99 <pachum99@gmail.com>
2019-02-24 21:56:47 -0800

    Initial conway code

--- conway.py
+++ conway.py
@@ -1,19 +1,60 @@
 import random
+from apcs import *
 
 map = []
 
-size = [5,5]
-for i in range(size[0]):
-    nrow = []
-    for n in range(size[1]):
-        l = random.randint(1,101)
-        if l <= 50:
-            nrow.append(False)
-        else:
-            nrow.append(True)
-    map.append(nrow)
+size = [50, 50]
+Window.size(size[0]*10, size[1]*10)
+#Window.size(500,500)
 
-while True:
-    for l in map:
-        for i in map:
-           print(l,i)
+for i in range(size[0]):
+    crow = []
+    for n in range(size[1]):
+        crow.append(bool(random.getrandbits(1)))
+    map.append(crow)
+
+def main():
+
+    bgID = Window.out.background("white")
+    mapID = []
+    rc = 0
+    for r in map:
+        ic = 0
+        crow = []
+        for i in r:
+
+
+            if i:
+                Window.out.color("black")
+            else:
+                Window.out.color("white")
+            crow.append(Window.out.square(ic*10+5, rc*10+5, 10))
+            ic += 1
+
+        mapID.append(crow)
+        rc += 1
+
+    if Window.mouse.clicked():
+        ic,rc = int(Window.mouse.getX()/10), int(Window.mouse.getY()/10)
+        count = 0
+        if map[ic - 1][rc - 1]:
+            count += 1
+        if map[ic][rc - 1]:
+            count += 1
+        if map[ic + 1][rc - 1]:
+            count += 1
+        if map[ic + 1][rc]:
+            count += 1
+        if map[ic + 1][rc + 1]:
+            count += 1
+        if map[ic][rc + 1]:
+            count += 1
+        if map[ic - 1][rc + 1]:
+            count += 1
+        if map[ic - 1][rc]:
+            count += 1
+        print(count)
+
+
+Window.frame(main)
+Window.start()
